8bf17f95a1fbdd54fb2cbd4a08e73fead73c7c6c
rem <reymartcenteno03@gmail.com>
2024-11-18 11:08:46 +0800

    Update index.py

--- pyp/index.py
+++ pyp/index.py
@@ -10,38 +10,43 @@ from colorama import Fore, Back, Style
 from dataModule import command_functions
 from pyptools import *
 
-config={
+config = {
   "user": "admin",
   "dbName": "data.json"
 }
 
 def updateUser():
-  with open("data.json", 'r') as file:
-    file.seek(0)
-    data=json.load(file)
-    config["user"] = data["Author"]
-    file.close()
+    try:
+        with open(config["dbName"], 'r') as file:
+            data = json.load(file)  # Read and load the JSON file
+            config["user"] = data["Author"]
+    except FileNotFoundError:
+        print("Error: JSON file not found.")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print("Error: Failed to parse JSON file.")
+        sys.exit(1)
 
 def function(str, args):
-  command_functions[str](str, args)
+    command_functions[str](str, args)
   
 def notFunction(str, args):
-  return False
+    return False
 
-decision={
-  True: function,
-  False: notFunction
+decision = {
+    True: function,
+    False: notFunction
 }
+
 def input_():
-  updateUser()
-  inp = input(colortext(config["user"],"yellow")+colortext(" ~ ","green"))
-  split = inp.split(" ")
-  args=[]
-  check=(split[0] in command_functions) or False
-  for x in range(len(split)):
-  	args.append(split[x].lower())
-  decision[check](split[0], args)
-  input_()
-    
-    
+    updateUser()
+    inp = input(colortext(config["user"], "yellow") + colortext(" ~ ", "green"))
+    split = inp.split(" ")
+    args = []
+    check = (split[0] in command_functions) or False
+    for x in range(len(split)):
+        args.append(split[x].lower())
+    decision[check](split[0], args)
+    input_()
+
 input_()
